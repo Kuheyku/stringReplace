167150cf691a80053f2d46f1ebf9fa3571ea3da4
Kuheyku <53658723+Kuheyku@users.noreply.github.com>
2020-11-03 18:57:29 +0800

    string replacer 2nd commit

--- stringReplace.py
+++ stringReplace.py
@@ -3,10 +3,11 @@
 #Title: stringReplacer
 #Purpose: To Replace Strings in multiple Files
 #Python version: 3.8.6 64-bit
+#Program version: 1.0
 #FOR TESTING PURPOSES
 #LAPTOP-B1P29PK\SQLEXPRESS2016
 #Janmo_POS_db
-import os, json
+import json
 
 def writeToConfig(dataAdd):
     with open("config.json", "w") as outfile:
@@ -19,66 +20,97 @@ def openConfig():
         configFile.close()
     return data
 
-def fileReplace(stringChangeDS, currentStringDS, stringChangeDB, currentStringDB, filename):
+def fileReplace(currentString, stringChange, filename):
     # Read in the file
     with open(filename, 'r', encoding='utf-8') as file :
         filedata = file.read()
     # Replace the target string with the new string
-    filedata = filedata.replace(currentStringDS, stringChangeDS)
-    filedata = filedata.replace(currentStringDB, stringChangeDB)
+    filedata = filedata.replace(currentString, stringChange)
     # Write the file out again
     with open(filename, 'w', encoding='utf-8') as file:
         file.write(filedata)
 
+# This function will append dictionary key:value into the router list
+def appendList(instanceName, currentStringList, changeStringList, pathLocationList, data):
+    data['instance'].append({
+        "instanceName": instanceName,
+        "currentStringList": currentStringList,
+        "changeStringList": changeStringList,
+        "pathLocationList": pathLocationList
+    })
+    return data
+
 def setup():
-    loc0 = "MainFnB/App.ini"
-    loc1 = "MecWise.JobServiceV4/Jobs/connectInfo.config"
-    loc2 = "MecWise.JobServiceV4/Jobs/MecWise.JobLauncherV41.exe.config"
-    loc3 = "C:/MecWisePOS/MecWisePOS.exe.config"
-    print("Introduction: Autoscript for F&B software by Aaron Lee!")
-    print("Version: 1.0")
-    print("Instructions:\nDeploy this script in the directory \'D:\\\' drive")
-    print("\nPlease check if local directory path is correct:\n"+loc0+"\n"+loc1+"\n"+loc2+"\n"+loc3)
-    userInput = input("Press enter to continue...")
-    os.system("cls")
+    # 1. Check whether a config file is present if not intialize variables accordingly
+    try:
+        data = openConfig()
+        instance = len(data["instance"])
+    except:
+        data = {}
+        data["instance"] = []
+        instance = 0
+    currentStringList = []
+    changeStringList = []
+    pathLocationList = []
     userInput = ""
     while userInput == "":
-        userInput = input("What is the name of the data source?(Example: DESKTOP-B5PK6DK\SQLEXPRESS2008)\n")
-    stringChangeDS = userInput
-    userInput = ""
-    userInput = input("\nWhat is the name of the data source in the config file?\nLeave input blank to use default values (Default: DESKTOP-B5PK6DK\SQLEXPRESS2008)\n")
-    if userInput == "":
-        currentStringDS = "DESKTOP-B5PK6DK\SQLEXPRESS2008"
-    else:
-        currentStringDS = userInput
-    print(currentStringDS)
-    userInput = ""
-    while userInput == "":
-        userInput = input("\nWhat is the name of the current database?(Example: FnB_Pos_Demo)\n")
-    stringChangeDB = userInput
-    userInput = ""
-    userInput = input("\nWhat is the name of the current database in the config file?\nLeave input blank to use default values(Default: FnB_Pos_Demo)\n")
-    if userInput == "":
-        currentStringDB = "FnB_Pos_Demo"
-    else:
-        currentStringDB = userInput
-    fileReplace(stringChangeDS, currentStringDS, stringChangeDB, currentStringDB, loc0)
-    fileReplace(stringChangeDS, currentStringDS, stringChangeDB, currentStringDB, loc1)
-    fileReplace(stringChangeDS, currentStringDS, stringChangeDB, currentStringDB, loc2)
-    fileReplace(stringChangeDS, currentStringDS, stringChangeDB, currentStringDB, loc3)
+        userInput=input("Please input the name of this replace string instance\n")
+    instanceName = userInput
+    # 2. Ask for current string/text to be replaced with new string/text
+    while userInput != "":
+        userInput=input("Please input the string/text to be replaced. [CASE-SENSITIVE]\n(Leave input empty if there is none left to change)\n")
+        if userInput != "":
+            currentStringList.append({"string":userInput})
+            userInput = ""
+            while userInput == "":
+                userInput=input("Please input in the new string/text to be replacing that string/text. [CASE-SENSITIVE]\n")
+            changeStringList.append({"string":userInput})
+    userInput = "init"
+    # 3. Ask for file pathnames to apply changes to
+    while userInput != "":
+        userInput=input("Please input the path name of the files for the changes to be applied to. [CASE-SENSITIVE]\n(Leave input empty if there is no files left to apply changes to)\n")
+        if userInput != "":
+            pathLocationList.append({"path":userInput})
+
+    writeToConfig(appendList(instanceName, currentStringList, changeStringList, pathLocationList, data))
 
 def run():
+    try:
+        data = openConfig()
+        instanceAmt = len(data["instance"])
+        currentInstance = 0
+        for x in data["instance"]:
+            try:
+                # First it initializes all the variables
+                instanceName = data["instance"][currentInstance][currentInstance]
+                currentStringList = data["instance"][currentInstance][currentStringList]
+                changeStringList = data["instance"][currentInstance][changeStringList]
+                pathLocationList = data["instance"][currentInstance][pathLocationList]
+                print("Running string replace for instance:" + instanceName)
+                pointer = len(currentStringList) - 1
+                pointerPath = len(pathLocationList) - 1
+                while pointer >= 0:
+                    def fileReplace(currentStringList["string"][pointer]
+                    pointer = pointer - 1
+                currentInstance = currentInstance + 1
+
+                
+            except:
+                print("[ERROR] Config file is corrupted. Please ensure all variables are correct in the file named \'config.json\' present in the same directory.\nIf error persists please delete \'config.json\' and set up all string replace instances.")
+    except:
+        print("[ERROR] Config file not found. Ensure a file named \'config.json\' is present in the same directory.")
 
 def main():
     choice = 0
-    while choice = 0:
+    print("Introduction: Light-Weight String Replacer Program by Aaron Lee!")
+    print("Program version: 1.0")
+    while choice == 0:
         # Ask user for their intention
-        userInput=input("String Replacer\nPlease pick a function\n(1) Add new instance\n(2) Run stored instance\nChoice: ")
+        userInput=input("\nPlease pick a function\n(1) Add new string replace instance\n(2) Run stored string replace instance\nChoice: ")
         if userInput == "1":
-            setup(True)
+            setup()
             break
         elif userInput == "2":
-            print("Run Password Changer")
             run()
             break
         else:
